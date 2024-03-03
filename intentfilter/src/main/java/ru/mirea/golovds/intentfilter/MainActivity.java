df3c8bfb62dd61126646f6c0ddd1fba5426bd97c
Azmodessu <golovrkd@yandex.ru>
2024-03-03 23:52:51 +0300

    toast

---
+++ intentfilter/src/main/java/ru/mirea/golovds/intentfilter/MainActivity.java
@@ -0,0 +1,37 @@
+package ru.mirea.golovds.intentfilter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast toast = Toast.makeText(getApplicationContext(),
+                "Здравствуй MIREA!",
+                Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void onClick(View view) {
+        Uri address = Uri.parse("https://www.mirea.ru/");
+        Intent openLinkIntent = new Intent(Intent.ACTION_VIEW, address);
+        startActivity(openLinkIntent);
+    }
+
+    public void onClickSend(View view) {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_SUBJECT, "MIREA");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, "Голов Денис Сергеевич");
+        startActivity(Intent.createChooser(shareIntent, "МОИ ФИО"));
+    }
+}
